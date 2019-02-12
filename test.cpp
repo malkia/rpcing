59006af42acd0436e393a8efe417a534c4027aa3
Dimiter 'malkia' Stanev <malkia@gmail.com>
2019-02-12 01:27:31 +0000

    more async expreriment testing

--- test.cpp
+++ test.cpp
@@ -114,9 +114,11 @@ public:
     // For each Write, a tag must be received from the completion queue, before procedding with another.
     for( auto& requestWithResponse : requestsWithResponses )
     {
+      //printf("@"); fflush(stdout);
       rpc->Write( requestWithResponse.request, (void*)&requestWithResponse.request );
       if( !completionQueue.Next(&tag, &ok) || !ok || tag != (void*)&requestWithResponse.request )
         printf("failed, tag=%p\n", tag );
+      //printf("#"); fflush(stdout);
       rpc->Read( &requestWithResponse.response, (void*)&requestWithResponse.response );
       if( !completionQueue.Next(&tag, &ok) || !ok || tag != (void*)&requestWithResponse.response )
         printf("failed, tag=%p\n", tag );
@@ -228,18 +230,84 @@ class CommandService : public service::MainControl::Service {
      size_t writeCount = 0;
      for( ;; )
      {
-     //   printf(" A\n");
+        printf(" A\n");
         if( !stream->Read(&commandRequest) )
           break;
         readCount++;
-   //     printf(" B\n");
+        printf(" B\n");
        //commandResponse.set_result( toastedString );
       if( !stream->Write(commandResponse) )
          printf(" w! !");
       writeCount++;
      }
      printf(" C readCount=%zd writeCount=%zd\n", readCount, writeCount );
+     exit(0);
      return grpc::Status::OK;
+  }
+};
+
+
+class CallData {
+  grpc::ServerContext m_serverContext;
+  grpc::ServerAsyncReaderWriter<service::CommandResponse, service::CommandRequest> m_serverStream;
+  service::MainControl::AsyncService* m_service;
+  service::CommandRequest m_commandRequest;
+  grpc::CompletionQueue* m_cq;
+  grpc::ServerCompletionQueue* m_serverCQ;
+  enum class State : uint8_t {
+    INITIAL,
+    READ,
+    WRITE,
+    FINISHED
+  };
+  State m_state{ State::INITIAL };
+public:
+  CallData( service::MainControl::AsyncService *service, grpc::ServerCompletionQueue* cq )
+    : m_serverStream( &m_serverContext )
+    , m_service( service )
+    , m_cq( cq )
+    , m_serverCQ( cq )
+  {
+    printf("aaa1\n");
+      service->RequestCommandStream( &m_serverContext, &m_serverStream, m_cq, m_serverCQ, this );
+    printf("aaa2\n");
+  }
+
+  void Next() 
+  {
+    switch( m_state ) 
+    {
+        default:
+        case State::FINISHED:
+          printf("."); fflush(stdout);
+          delete this;
+          return;
+
+        case State::INITIAL:
+          // printf("I"); fflush(stdout);
+          m_state = State::READ;
+          m_serverStream.SendInitialMetadata( this );
+          return;
+
+        case State::READ:
+          //  printf("r"); fflush(stdout);
+          m_state = State::WRITE;
+          m_serverStream.Read( &m_commandRequest, this );
+          return;
+  
+        case State::WRITE:
+          //printf("W"); fflush(stdout);
+          m_state = State::READ;
+          service::CommandResponse response;
+          m_serverStream.Write( response, this );
+          return;
+    }
+  }
+
+  void Stop()
+  {
+    m_state = State::FINISHED;
+    m_serverStream.Finish( grpc::Status::OK, this );
   }
 };
 
@@ -249,6 +317,31 @@ static void serverThreadProc( std::unique_ptr<grpc::Server> server )
   server->Wait();
   printf("About to finish\n");
 }
+
+
+static void asyncServerThreadProc( service::MainControl::AsyncService *service, std::unique_ptr<grpc::ServerCompletionQueue> serverCQ, std::string serverId )
+{
+  printf("About to start\n");
+  new CallData( service, serverCQ.get() );
+  void* tag;
+  bool ok;
+  for( ;; )
+  {
+    bool shouldContinue = serverCQ->Next( &tag, &ok );
+    printf("%s: %c%c\n", serverId.c_str(), shouldContinue ? '.' : '?', ok ? '+' : '-' ); fflush(stdout);
+    auto callData = static_cast<CallData*>(tag);
+   // fflush(stdout);
+    //printf( "asyncServer: shouldContinue=%d ok=%d tag=%p\n", shouldContinue, ok, tag );
+    if( !ok )
+      callData->Stop();
+    callData->Next();
+    if( !ok )
+      break;
+    std::this_thread::sleep_for( std::chrono::milliseconds(rand()%1000) );
+  }
+  printf("About to finish\n");
+}
+
 
 int main( int argc, const char* argv[] )
 {
@@ -263,19 +356,35 @@ int main( int argc, const char* argv[] )
   clientResourceQuota.Resize( 1024*1024*16 );//* 1024 * 1024 );
 
   grpc::ServerBuilder serverBuilder;
-  serverBuilder.SetResourceQuota( serverResourceQuota );
-  serverBuilder.SetDefaultCompressionAlgorithm(GRPC_COMPRESS_NONE);
+ // serverBuilder.SetResourceQuota( serverResourceQuota );
+ // serverBuilder.SetDefaultCompressionAlgorithm(GRPC_COMPRESS_NONE);
   serverBuilder.AddListeningPort( serverAddr, grpc::InsecureServerCredentials() );
-  CommandService theService;
+  service::MainControl::AsyncService theService;
   serverBuilder.RegisterService( &theService );
-  std::thread( serverThreadProc, std::move(serverBuilder.BuildAndStart()) ).detach();
+  auto serverCompletionQueue1 = serverBuilder.AddCompletionQueue();
+  //auto serverCompletionQueue2 = serverBuilder.AddCompletionQueue();
+  //auto serverCompletionQueue3 = serverBuilder.AddCompletionQueue();
+  //auto serverCompletionQueue4 = serverBuilder.AddCompletionQueue();
+  //auto serverCompletionQueue5 = serverBuilder.AddCompletionQueue();
+  auto server = serverBuilder.BuildAndStart();
 
   grpc::ChannelArguments channelArguments;
-  channelArguments.SetResourceQuota( clientResourceQuota );
-  channelArguments.SetCompressionAlgorithm(GRPC_COMPRESS_NONE);
+//  channelArguments.SetResourceQuota( clientResourceQuota );
+//  channelArguments.SetCompressionAlgorithm(GRPC_COMPRESS_NONE);
+#if 1
+  CommandClient commandClient(server->InProcessChannel(channelArguments));
+#else
   CommandClient commandClient(grpc::CreateCustomChannel(
     serverAddr, grpc::InsecureChannelCredentials(), channelArguments
   ));
+#endif
+
+//  std::thread( serverThreadProc, std::move(server) ).detach();
+  auto st1{ std::thread( asyncServerThreadProc, &theService, std::move(serverCompletionQueue1), std::string("1") ) };
+  //auto st2{ std::thread( asyncServerThreadProc, &theService, std::move(serverCompletionQueue2), std::string("2") ) };
+  //auto st3{ std::thread( asyncServerThreadProc, &theService, std::move(serverCompletionQueue3), std::string("3") ) };
+  //auto st4{ std::thread( asyncServerThreadProc, &theService, std::move(serverCompletionQueue4), std::string("4") ) };
+  //auto st5{ std::thread( asyncServerThreadProc, &theService, std::move(serverCompletionQueue5), std::string("5") ) };
 
   //int counter = 0;
   std::string testString{ "test" };
@@ -284,7 +393,7 @@ int main( int argc, const char* argv[] )
   grpc::CompletionQueue completionQueue;
   grpc::ClientContext clientContext;
   service::CommandRequest request;
-  size_t batchSize = 16384*4;
+  size_t batchSize = 4;//16384*4;
   std::vector<RequestWithResponse> requestsWithResponses;
   auto t10 = std::chrono::steady_clock::now();
   requestsWithResponses.resize( batchSize );
@@ -292,6 +401,7 @@ int main( int argc, const char* argv[] )
   commandClient.IssueAsyncCommandStream( clientContext, requestsWithResponses, completionQueue );
   auto t11 = std::chrono::steady_clock::now();
   printf( "init                time=%20.8f\n", (t11-t10).count()/1e9);
+
 #elif 1
   grpc::CompletionQueue completionQueue;
   service::CommandRequest request;
@@ -324,6 +434,12 @@ int main( int argc, const char* argv[] )
   }
 #endif
   auto t2 = std::chrono::steady_clock::now();
-  printf( "%20.8f\n", (t2-t1).count()/1e9);
+  printf( "end %20.8f\n", (t2-t1).count()/1e9);
+
+  st1.join();
+  //st2.join();
+  //st3.join();
+  //st4.join();
+  //st5.join();
   return 0;
 }
