e64087b10b61ff72a8c6e15ece9ac3a0dacd79ac
Dimiter 'malkia' Stanev <malkia@gmail.com>
2019-02-13 15:03:54 +0000

    golang protobuf shenanigans

--- ken.go
+++ ken.go
@@ -1,7 +1,28 @@
 package main
 
 import "fmt"
+import service "github.com/malkia/rpcing"
+import "github.com/golang/protobuf/proto"
 
 func main() {
-    fmt.Println("hello world")
+    x := service.CommandRequest{}
+    y := service.CommandResponse{}
+    data, err := proto.Marshal(&x);
+    if err != nil {
+		fmt.Println("%v", err);
+    }
+    var z service.CommandRequest
+	err = proto.Unmarshal(data, &z)
+	if err != nil {
+		fmt.Println("%v", err);
+	}
+    fmt.Println("hello world %+v", x)
+    fmt.Println("hello world %+v", y)
+    fmt.Println("hello world %+v", z)
+    a := service.MainControlServer(nil)
+	b := service.MainControlClient(nil)
+	(&service.CommandRequest{}).Marshal()
+    (&service.CommandResponse{}).Marshal()
+    fmt.Println("a=%+v", a);
+    fmt.Println("b=%+v", b);
 }
